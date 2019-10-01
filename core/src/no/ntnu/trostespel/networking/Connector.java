d8de6501b8471237db7f6db583a4aa5ac0d9fcd6
mknauser <33400600+mknauser@users.noreply.github.com>
2019-10-01 14:11:32 +0200

    Added a new interface ServerConnector, which is implemented by an abstract class CommunicationManager. Also includes new class Connector which implements the method connect(), from the ServerConnector interface. Fix #3

--- core/src/no/ntnu/trostespel/networking/Connector.java
+++ core/src/no/ntnu/trostespel/networking/Connector.java
@@ -3,6 +3,14 @@ package no.ntnu.trostespel.networking;
 public class Connector extends CommunicationManager {
     @Override
     public String connect() {
-        return null;
+        String result;
+
+        try {
+
+        } catch () {
+
+        }
+
+        return result;
     }
 }
