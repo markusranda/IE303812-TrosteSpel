2ad160f7ea3736bf3a79514e338d1538800cd88e
mknauser <33400600+mknauser@users.noreply.github.com>
2019-10-08 14:34:59 +0200

    GameState now knows about GameObjects

--- gamelibrary/src/no/ntnu/trostespel/GameState.java
+++ gamelibrary/src/no/ntnu/trostespel/GameState.java
@@ -1,11 +1,33 @@
 package no.ntnu.trostespel;
 
+import no.ntnu.trostespel.entity.GameObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class GameState <T> {
 
     public static final int playerSpeed = 100;
 
     public HashMap<Long, T> players;
+    private List<GameObject> entities;
+
+    // static variable single_instance of type Singleton
+    private static GameState single_instance = null;
+
+    public static GameState getInstance() {
+        if (single_instance == null) {
+            single_instance = new GameState();
+        }
+        return single_instance;
+    }
+
+    private GameState() {
+        entities = new ArrayList<>();
+    }
+
+    public List<GameObject> getEntities() {
+        return entities;
+    }
 }
