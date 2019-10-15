3fdbbbb4882d222469e55e70d82ffc0a6aed44de
mknauser <33400600+mknauser@users.noreply.github.com>
2019-10-15 14:57:12 +0200

    Connection now includes a CircularArrayList this is will be used to contain all the GameStates on client has on the Server.

--- server/src/main/java/no/ntnu/trostespel/model/Connection.java
+++ server/src/main/java/no/ntnu/trostespel/model/Connection.java
@@ -1,15 +1,24 @@
 package no.ntnu.trostespel.model;
 
+import no.ntnu.trostespel.CircularArrayList;
+
 import java.net.InetAddress;
 
 public class Connection {
 
+    private static final int SNAPSHOP_ARRAY_MAX_SIZE = 32;
     private InetAddress address;
     private long playerId;
+    private CircularArrayList snapshotArray;
 
     public Connection(InetAddress address, long playerId) {
         this.address = address;
         this.playerId = playerId;
+        this.snapshotArray = new CircularArrayList(SNAPSHOP_ARRAY_MAX_SIZE);
+    }
+
+    public CircularArrayList getSnapshotArray() {
+        return snapshotArray;
     }
 
     public InetAddress getAddress() {
