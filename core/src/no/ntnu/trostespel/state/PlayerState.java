79b3d42f6f36740a0d88f157306c6076b95463c0
mknauser <33400600+mknauser@users.noreply.github.com>
2019-10-31 14:13:58 +0100

    Dead player can no longer move.

--- core/src/no/ntnu/trostespel/state/PlayerState.java
+++ core/src/no/ntnu/trostespel/state/PlayerState.java
@@ -98,4 +98,8 @@ public class PlayerState extends ObjectState{
     public Action getAction() {
         return action;
     }
+
+    public boolean isDead() {
+        return action == Action.DEAD;
+    }
 }
