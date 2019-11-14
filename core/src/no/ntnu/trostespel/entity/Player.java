9ae5a4e8e7295b559e5e82224611e4b60e3764fb
Markus Randa <randamarkus.95@gmail.com>
2019-11-14 14:26:00 +0100

    Playername looks a bit better.
    It's really limited what one can do when drawing a font
    straight into the game batch it seems.

--- core/src/no/ntnu/trostespel/entity/Player.java
+++ core/src/no/ntnu/trostespel/entity/Player.java
@@ -2,10 +2,7 @@ package no.ntnu.trostespel.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import no.ntnu.trostespel.config.Assets;
